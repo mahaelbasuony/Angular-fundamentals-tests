ff0c96d7efdb8fbdd0cad3c359e8b25001d07f05
Maha Elbasuony <mahaelbasuony@gmail.com>
2017-11-16 17:20:44 +0100

    Retrieved data using  HTTP by observable and subsrcibe

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number  = 50;
   imageMargin: number = 2;
   showImage: boolean  = false;
+  errorMessage: string;
 
   _listFilter: string  ;
   get listFilter():string{
@@ -38,8 +39,14 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit():void{
-    this.products =this._productService.getProducts();
-    this.filteredProducts=this.products;
+    this._productService.getProducts()
+    .subscribe(products =>
+      { 
+        this.products= products;
+        this.filteredProducts=this.products;
+        
+      },error => this.errorMessage= <any>error);
+    
   }
 
   onRatingClicked(message: string):void{
